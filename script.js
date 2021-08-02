c2b848479a458635847cab10cf88783d368c3eab
aakash-s45 <tech2aks@gmail.com>
2021-08-03 01:18:15 +0530

    added random array

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const arr = [1, 5, 8, 9, 47, 55, 96, 25, 36, 45];
+let arr = [];
+let len = 30;
+while(arr.length < len){
+    let r = Math.floor(Math.random() * 300) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
 let pag = document.getElementById("arr");
-let len=arr.length;
 let b=window.innerWidth;
 let wid=0.7*b/len;
 for (let i = 0; i < arr.length; i++) {
-    pag.innerHTML += "<div class='elem' style='height:" + arr[i] * 3 + "px'>" + arr[i] + "</div>";
+    pag.innerHTML += "<div class='elem' style='height:" + arr[i]*2 + "px'>" + arr[i] + "</div>";
     // console.log(i);
 }
 let x = document.getElementById("arr");
