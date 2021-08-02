0577f2700b883a20f6e9d10098ca27b229b7a7f4
aakash-s45 <tech2aks@gmail.com>
2021-08-03 01:09:18 +0530

    renamed array from 'bars' to 'arr'

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const bars = [1, 5, 8, 9, 47, 55, 96, 25, 36, 45];
-// let pag = document.getElementsByClassName("arr");
+const arr = [1, 5, 8, 9, 47, 55, 96, 25, 36, 45];
 let pag = document.getElementById("arr");
-let len=bars.length;
+let len=arr.length;
 let b=window.innerWidth;
-let wid=0.9*b/len;
-// console.log(wid);
-for (let i = 0; i < bars.length; i++) {
-    // pag.innerHTML+="hello"+bars[i]+"<br>";
-    pag.innerHTML += "<div class='elem' style='height:" + bars[i] * 3 + "px'>" + bars[i] + "</div>";
-    console.log(i);
+let wid=0.7*b/len;
+for (let i = 0; i < arr.length; i++) {
+    pag.innerHTML += "<div class='elem' style='height:" + arr[i] * 3 + "px'>" + arr[i] + "</div>";
+    // console.log(i);
 }
 let x = document.getElementById("arr");
 let y = x.getElementsByClassName("elem");
