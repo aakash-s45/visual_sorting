65c15368d2ae2b24fcb36eb1d6a706eab9f4b41b
aakash-s45 <tech2aks@gmail.com>
2021-08-04 19:00:20 +0530

    sorting'

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 let arr = [];
 let len = 10;
-// let len;
 
-
-
-
-function myfun(){
-  document.getElementById('arr').innerHTML="";
-  arr=[];
+function myfun() {
+  document.getElementById('arr').innerHTML = "";
+  arr = [];
 }
 
 function randomArray() {
@@ -21,7 +17,7 @@ function randomArray() {
   let b = window.innerWidth;
   let wid = 0.7 * b / len;
   for (let i = 0; i < arr.length; i++) {
-    pag.innerHTML += "<div class='elem' style='height:" + arr[i] * 2 + "px'>" + arr[i] + "</div>";
+    pag.innerHTML += "<div class='elem' style='height:" + arr[i] + "px'>" + arr[i] + "</div>";
   }
   let x = document.getElementById("arr");
   let y = x.getElementsByClassName("elem");
@@ -38,30 +34,43 @@ function slider2len() {
   slider.oninput = function () {
     len = this.value;
     console.log(len);
-    // return this.value;
   }
 }
 slider2len();
-// len=slider2len();
 
 let generateButton = document.getElementById("genbtn");
-generateButton.addEventListener("onclick",()=>{console.log("clicked btn");});
+generateButton.addEventListener("onclick", () => { console.log("clicked btn"); });
 let slideVal = document.getElementById("slider");
 slideVal.addEventListener("change", randomArray);
-console.log(arr);
 
-function selectionSort() { 
-  let arrlenght = arr.length;
+sortedArray = arr;
+console.log(sortedArray);
 
-  for(let first=0; first<arrlenght-1; first++){
-  for(let second = first+1; second <arrlenght; second++){
-    if(arr[second] < arr[first]){
-      let temp= arr[second];
-      arr[second]=arr[first];
-      arr[first]=temp;
+function selectionSort(sortedArray) {
+  let arrlenght = sortedArray.length;
+  for (let first = 0; first < arrlenght - 1; first++) {
+    for (let second = first + 1; second < arrlenght; second++) {
+      if (sortedArray[second] < sortedArray[first]) {
+        let temp = sortedArray[second];
+        sortedArray[second] = sortedArray[first];
+        sortedArray[first] = temp;
+      }
     }
   }
+  console.log("sorting done");
+  console.log(sortedArray);
 }
+
+function changeArray() {
+  console.log(len);
+  let arrayHTML = document.getElementById("arr");
+  let arrayElements = arrayHTML.getElementsByClassName("elem");
+  console.log(arrayElements.length);
+  let lenghtarray =arrayElements.length;
+
+  for (let i = 0; i < lenghtarray; i++) {
+    arrayElements[i].style.height = sortedArray[i]+'px';
+    // console.log(arrayElements[i].style.height );
+    // console.log(sortedArray[i] * 2 + "px");
+  }
 }
-selectionSort(arr);
-console.log(arr);
