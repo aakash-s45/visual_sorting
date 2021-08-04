6892674d31e9e9dffd539d5c0f73c430eb8095c2
hritik-chouhan <hritikc555@gmail.com>
2021-08-04 17:25:14 +0530

    selection sort

--- script.js
+++ script.js
@@ -51,3 +51,26 @@ generateButton.addEventListener("onclick",()=>{console.log("clicked btn");});
 let slideVal = document.getElementById("slider");
 slideVal.addEventListener("change", randomArray);
 // slideVal.addEventListener("mousedown",myfun);
+console.log(arr);
+
+function selectionSort() { 
+  let arrlenght = arr.length;
+
+  for(let first=0; first<arrlenght-1; first++){
+  for(let second = first+1; second <arrlenght; second++){
+    if(arr[second] < arr[first]){
+      let temp= arr[second];
+      arr[second]=arr[first];
+      arr[first]=temp;
+      
+    }
+    
+  }
+}
+
+
+  
+
+}
+selectionSort(arr);
+console.log(arr);
