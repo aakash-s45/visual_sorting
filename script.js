49b89dc9d4d967102907bf82691b71cbaf0471be
hritik-chouhan <hritikc555@gmail.com>
2021-08-04 19:06:06 +0530

    merge sort and bubble sort are implemented

--- script.js
+++ script.js
@@ -72,5 +72,66 @@ function selectionSort() {
   
 
 }
-selectionSort(arr);
-console.log(arr);
+
+function bubble_sort(arr){
+     
+  for(var ft = 0; ft < arr.length; ft++){
+      
+     
+    for(var st = 0; st < ( arr.length - ft -1 ); st++){
+        
+      
+      
+      if(arr[st] > arr[st+1]){
+          
+        
+        var temp = arr[st]
+        arr[st] = arr[st + 1]
+        arr[st+1] = temp
+      }
+    }
+  }
+  
+  
+ }
+
+ let arr_merge = arr
+
+ function merge(left, right) {
+  let arr = []
+  // Break out of loop if any one of the array gets empty
+  while (left.length && right.length) {
+      // Pick the smaller among the smallest element of left and right sub arrays 
+      if (left[0] < right[0]) {
+          arr.push(left.shift())  
+      } else {
+          arr.push(right.shift()) 
+      }
+  }
+  
+  // Concatenating the leftover elements
+  // (in case we didn't go through the entire left or right array)
+  return [ ...arr, ...left, ...right ]
+}
+
+function mergeSort(array) {
+const half = array.length / 2
+
+// Base case or terminating case
+if(array.length < 2){
+  return array 
+}
+
+const left = array.splice(0, half)
+return merge(mergeSort(left),mergeSort(array))
+}
+ 
+arr_merge = mergeSort(arr_merge);
+console.log(arr_merge);
+
+
+
+
+
+
+
