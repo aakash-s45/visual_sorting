6ac1d9eed8a2590d0cb263bc048201f2b3d378de
aakash-s45 <tech2aks@gmail.com>
2021-08-05 16:31:37 +0530

    added gitignore

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let arr = [];
 let len = 10;
 
+function slider2len() {
+  var slider = document.getElementById("slider");
+  slider.oninput = function () {
+    len = this.value;
+    console.log(len);
+  }
+}
+slider2len();
+
 function myfun() {
   document.getElementById('arr').innerHTML = "";
   arr = [];
@@ -17,7 +26,7 @@ function randomArray() {
   let b = window.innerWidth;
   let wid = 0.7 * b / len;
   for (let i = 0; i < arr.length; i++) {
-    pag.innerHTML += "<div class='elem' style='height:" + arr[i] + "px'>" + arr[i] + "</div>";
+    pag.innerHTML += "<div class='elem' style='height:" + arr[i] * 2 + "px'>" + "</div>";
   }
   let x = document.getElementById("arr");
   let y = x.getElementsByClassName("elem");
@@ -25,52 +34,52 @@ function randomArray() {
   for (i = 0; i < y.length; i++) {
     y[i].style.width = wid + "px";
   }
-}
-
-
-randomArray();
-function slider2len() {
-  var slider = document.getElementById("slider");
-  slider.oninput = function () {
-    len = this.value;
-    console.log(len);
-  }
-}
-slider2len();
-
-let generateButton = document.getElementById("genbtn");
-generateButton.addEventListener("onclick", () => { console.log("clicked btn"); });
-let slideVal = document.getElementById("slider");
-slideVal.addEventListener("change", randomArray);
-
-sortedArray = arr;
-console.log(sortedArray);
-
-function selectionSort(sortedArray) {
-  let arrlenght = sortedArray.length;
-  for (let first = 0; first < arrlenght - 1; first++) {
-    for (let second = first + 1; second < arrlenght; second++) {
-      if (sortedArray[second] < sortedArray[first]) {
-        let temp = sortedArray[second];
-        sortedArray[second] = sortedArray[first];
-        sortedArray[first] = temp;
-      }
+  if (y.length < 12) {
+    for (i = 0; i < y.length; i++) {
+      y[i].innerHTML=arr[i];
     }
   }
-  console.log("sorting done");
-  console.log(sortedArray);
 }
 
-function changeArray() {
-  console.log(len);
+randomArray();
+let generateButton = document.getElementById("genbtn");
+let slideVal = document.getElementById("slider");
+slideVal.addEventListener("change", randomArray);
+let sortedArray = [];
+console.log(sortedArray);
+
+let arrayHTML = document.getElementById("arr");
+arrayHTML.addEventListener("change", () => {
+  console.log(sortedArray.length);
+  console.log(sortedArray);
+})
+
+
+function selectionSort() {
+  sortedArray = arr;
   let arrayHTML = document.getElementById("arr");
   let arrayElements = arrayHTML.getElementsByClassName("elem");
-  console.log(arrayElements.length);
-  let lenghtarray =arrayElements.length;
+  let arrlenght = sortedArray.length;
 
-  for (let i = 0; i < lenghtarray; i++) {
-    arrayElements[i].style.height = sortedArray[i]+'px';
-    // console.log(arrayElements[i].style.height );
-    // console.log(sortedArray[i] * 2 + "px");
+  for (let first = 0; first < arrlenght - 1; first++) {
+    for (let second = first + 1; second < arrlenght; second++) {
+      setTimeout(() => {
+        if (sortedArray[second] < sortedArray[first]) {
+          let temp = sortedArray[second];
+          sortedArray[second] = sortedArray[first];
+          arrayElements[second].style.height = sortedArray[first] * 2 + "px";
+          arrayElements[second].style.backgroundColor = "red";
+          if(arrlenght<12)arrayElements[second].innerHTML = sortedArray[first];
+          sortedArray[first] = temp;
+          arrayElements[first].style.height = temp * 2 + "px";
+          arrayElements[first].style.backgroundColor = "blue";
+          if(arrlenght<12)arrayElements[first].innerHTML = temp;
+        }
+      }, 200 * second)
+    }
   }
 }
+
+console.log("sorting done");
+console.log(sortedArray);
+
